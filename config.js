d944ed3a7d9a49da2676e4c2b91b8db70d0ab646
LustfulAnarchy <38855075+LustfulAnarchy@users.noreply.github.com>
2019-05-17 22:04:27 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-exports.TOKEN = 'MjkwODM1MTUwMTk4Mjc2MDk3.XNLUlw.72h8XF0df25gWUobfLs3FOzpY5M';
+exports.TOKEN = '';
 
 exports.PREFIX = '$';
 
-exports.GOOGLE_API_KEY = 'AIzaSyDcmJT1eHiWMJ5vsIyGTpyy_g5WMj0soac';
+exports.GOOGLE_API_KEY = '';
 
 exports.SERVERPREFIXES = {
   '576370141827432448' : '!', //Testing server
